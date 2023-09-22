159427311d282d0b72c008bf00d19bf304889db5
Arpit Nakrani <arpit.cdmi@gmail.com>
2023-09-22 16:24:09 +0530

    node

--- apidemo/Assets/test.cs
+++ apidemo/Assets/test.cs
@@ -8,12 +8,12 @@ using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
-    public GameObject prefab,rawImg,home,puzzle;
+    public GameObject prefab, home, puzzle,play,puzzle_img;
     public Transform parent;
     public GameObject[] btn = new GameObject[12];
     public Transform parent2;
     public GameObject[] btn2 = new GameObject[12];
-    public GameObject prefab2, rawImg2;
+    public GameObject prefab2;
 
     // Start is called before the firt frame update
     void Start()
@@ -30,18 +30,18 @@ public class test : MonoBehaviour
     IEnumerator getdata()
     {
         WWW web = new WWW("http://localhost:3000/all");
-        
+
         yield return web;
-      
+
         JSONArray jSONArray = (JSONArray)JSON.Parse(web.text);
 
         for (int i = 0; i < jSONArray.Count; i++)
         {
             btn[i] = Instantiate(prefab, parent);
-            
-            btn[i].transform.GetChild(1).GetComponent<Text>().text = jSONArray[i]["cat_name"] ;
+
+            btn[i].transform.GetChild(1).GetComponent<Text>().text = jSONArray[i]["cat_name"];
             string str = jSONArray[i]["_id"];
-            Debug.Log("test :"+str);
+            Debug.Log("test :" + str);
             btn[i].GetComponent<Button>().onClick.AddListener(() => StartCoroutine(getpuzzle(str)));
             WWW webImg = new WWW("http://localhost:3000/images/" + jSONArray[i]["Image"]);
             yield return webImg;
@@ -53,26 +53,10 @@ public class test : MonoBehaviour
 
     void onclick(string str)
     {
-
-        //StartCoroutine(getpuzzle(str));
-        int a = 12;
-        string[] randomWords = GenerateRandomWords(a);
-        Debug.Log(randomWords);
-        
+        StartCoroutine(getpuzzle(str));
 
     }
-    static string[] GenerateRandomWords(int count)
-    {
-        string[] randomWords = new string[count];
-        System.Random random = new System.Random();
 
-        for (int i = 0; i < count; i++)
-        {
-            randomWords[i] = Guid.NewGuid().ToString().Substring(0, 5); // Generate random 5-character words
-        }
-
-        return randomWords;
-    }
     IEnumerator getpuzzle(string str)
     {
 
@@ -88,13 +72,30 @@ public class test : MonoBehaviour
             btn2[i] = Instantiate(prefab2, parent2);
 
             btn2[i].transform.GetChild(1).GetComponent<Text>().text = jSON[i]["puzzle_name"].Value;
-            //string str = jSON[i]["_id"];
+            string pzl_id = jSON[i]["_id"];
             WWW webImg = new WWW("http://localhost:3000/images/" + jSON[i]["Image"]);
             yield return webImg;
             Texture2D texture = webImg.texture;
             btn2[i].transform.GetChild(0).GetComponent<RawImage>().texture = texture;
-            //btn[i].GetComponent<Button>().onClick.AddListener(() => (str));
+            btn2[i].GetComponent<Button>().onClick.AddListener(() => StartCoroutine(get_Single_puzzle(pzl_id)));
         }
+    }
+
+     IEnumerator get_Single_puzzle(string str)
+    {
+        Debug.Log(str);
+        home.SetActive(false);
+        puzzle.SetActive(false);
+        play.SetActive(true);
+        WWW GetSingle_puzzle = new WWW("http://localhost:3000/puzzle/" + str);
+        yield return GetSingle_puzzle;
+        JSONArray jSON = (JSONArray)JSON.Parse(GetSingle_puzzle.text);
+        Debug.Log(jSON[0]);
+        WWW get_img = new WWW("http://localhost:3000/images/" + jSON[0]["Image"]);
+        
+        yield return get_img;
+        Texture2D pzl_img = get_img.texture;
+        puzzle_img.GetComponent<RawImage>().texture = pzl_img;
     }
 
 
---
+++ puzzle (2)/puzzle/user/puzzle.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+const multer  = require('multer')
+mongoose.connect('mongodb://127.0.0.1:27017/puzzle_game')
+    .then(() => console.log('Connected!'));
+
+var puzzle = new mongoose.Schema({
+    cat_id: {
+        type: String
+    },
+    puzzle_name:{
+        type:String
+    },
+    Image:{
+        type:String
+    },
+    word:{
+        type:String
+    }
+
+})
+
+module.exports = mongoose.model('puzzle',puzzle)
